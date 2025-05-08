6c3eca5a506ebdcd83589c47b47a302223d13c14
Daniel Zornek <daniel_zornek91@outlook.com>
2025-05-08 16:17:06 -0300

    enviando projeto base

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template
+import redis
+
+red = redis.Redis(host='localhost', port=6379, db=0)
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template('index.html')
